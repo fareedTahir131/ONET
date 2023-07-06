92d024291f3b0c5712feea6c828727bb7f41e018
Fareed Tahir <fareedTahir131@gmail.com>
2023-07-06 09:24:23 +0500

    Language Dropdown and script working.
    Language Dropdown and script working.

--- Assets/Scripts/LanguageMananger.cs
+++ Assets/Scripts/LanguageMananger.cs
@@ -5,17 +5,128 @@ using UnityEngine.UI;
 
 public class LanguageMananger : MonoBehaviour
 {
+    public Dropdown LanguageDropdown;
+
     public Screen[] UI_Screens;
+
+    private string Language = "";
     void Start()
     {
-        TranslateText(0);
+        //TranslateText(0);
+        GetSelectedLanguage();
     }
-
-    public void TranslateText(int LanguageIndex)
+    public void OnLanguageChange()
     {
-        for (int i = 0; i < UI_Screens[LanguageIndex].UI_Texts.Length; i++)
+        Language = LanguageDropdown.options[LanguageDropdown.value].text.ToString();
+        PlayerPrefs.SetString("SelectedLang", Language);
+        GetSelectedLanguage();
+    }
+    public void GetSelectedLanguage()
+    {
+
+        if (PlayerPrefs.GetString("SelectedLang") == "")
         {
-            UI_Screens[LanguageIndex].UI_Texts[i].text = UI_Screens[LanguageIndex].language.Italian[i];
+            Language = LanguageDropdown.options[LanguageDropdown.value].text.ToString();
+            PlayerPrefs.SetString("SelectedLang", Language);
+        }
+        else
+        {
+            Language = PlayerPrefs.GetString("SelectedLang");
+        }
+        if (Language == "English")
+        {
+            Debug.Log("Selected Language is "+ Language);
+            TranslateTextToEnglish();
+        }
+        else if (Language == "Italian")
+        {
+            Debug.Log("Selected Language is " + Language);
+            TranslateTextToItalian();
+        }
+        else if (Language == "German")
+        {
+            Debug.Log("Selected Language is " + Language);
+            TranslateTextToGerman();
+        }
+        else if (Language == "French")
+        {
+            Debug.Log("Selected Language is " + Language);
+            TranslateTextToFrench();
+        }
+        else if (Language == "Spanish")
+        {
+            Debug.Log("Selected Language is " + Language);
+            TranslateTextToSpanish();
+        }
+        else if (Language == "Romanian")
+        {
+            Debug.Log("Selected Language is " + Language);
+            TranslateTextToRomanian();
+        }
+        else
+        {
+            Debug.Log("No Language Found");
+            TranslateTextToEnglish();
+        }
+    }
+    public void TranslateTextToEnglish()
+    {
+        for (int i = 0; i < UI_Screens.Length; i++)
+        {
+            for (int j = 0; j < UI_Screens[i].UI_Texts.Length; j++)
+            {
+                UI_Screens[i].UI_Texts[j].text = UI_Screens[i].language.English[j];
+            }
+        }
+    }
+    public void TranslateTextToItalian()
+    {
+        for (int i = 0; i < UI_Screens.Length; i++)
+        {
+            for (int j = 0; j < UI_Screens[i].UI_Texts.Length; j++)
+            {
+                UI_Screens[i].UI_Texts[j].text = UI_Screens[i].language.Italian[j];
+            }
+        }
+    }
+    public void TranslateTextToGerman()
+    {
+        for (int i = 0; i < UI_Screens.Length; i++)
+        {
+            for (int j = 0; j < UI_Screens[i].UI_Texts.Length; j++)
+            {
+                UI_Screens[i].UI_Texts[j].text = UI_Screens[i].language.German[j];
+            }
+        }
+    }
+    public void TranslateTextToFrench()
+    {
+        for (int i = 0; i < UI_Screens.Length; i++)
+        {
+            for (int j = 0; j < UI_Screens[i].UI_Texts.Length; j++)
+            {
+                UI_Screens[i].UI_Texts[j].text = UI_Screens[i].language.French[j];
+            }
+        }
+    }
+    public void TranslateTextToSpanish()
+    {
+        for (int i = 0; i < UI_Screens.Length; i++)
+        {
+            for (int j = 0; j < UI_Screens[i].UI_Texts.Length; j++)
+            {
+                UI_Screens[i].UI_Texts[j].text = UI_Screens[i].language.Spanish[j];
+            }
+        }
+    }
+    public void TranslateTextToRomanian()
+    {
+        for (int i = 0; i < UI_Screens.Length; i++)
+        {
+            for (int j = 0; j < UI_Screens[i].UI_Texts.Length; j++)
+            {
+                UI_Screens[i].UI_Texts[j].text = UI_Screens[i].language.Romanian[j];
+            }
         }
     }
 }
